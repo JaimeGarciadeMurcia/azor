19eabac3ba4909e1df1f6306dc909cdc83bbc2eb
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-06-11 09:04:11 +0200

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Dev shell dotnet 10";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs = { self, nixpkgs, ... }:
   let
